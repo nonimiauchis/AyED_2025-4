9404014f7920d9d3e25d6d8d7989a57039f1bb2f
PtnomuahC <chaumontpablo97@gmail.com>
2025-09-18 20:02:54 -0300

    Add files via upload

---
+++ 4_CHAUMONT_M/0_PC4_10/0_PC4_10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC6_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cree un programa en C# para convertir de grados centígrados a Kelvin y Fahrenheit. Solicite al usuario la cantidad de grados centígrados para convertirlos
+            Console.WriteLine("Ingrese los grados Celsius: ");
+            int cel = int.Parse(Console.ReadLine());
+            double Fah = (cel*9/5)+32;
+            Console.WriteLine("En grados Fahrenheit seria: " + Fah);
+            double Kel = cel+273.15;
+            Console.WriteLine("En grados Kelvin seria: " + Kel);
+            Console.ReadKey();
+        }
+    }
+}
