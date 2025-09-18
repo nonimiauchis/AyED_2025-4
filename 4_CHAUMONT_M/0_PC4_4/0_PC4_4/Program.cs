b8c15206bac5321144d0ab41ab36454ad0fa1733
PtnomuahC <chaumontpablo97@gmail.com>
2025-09-18 20:00:32 -0300

    Add files via upload

---
+++ 4_CHAUMONT_M/0_PC4_4/0_PC4_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC6_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Escribe un programa en C# que calcule el resultado de sumar dos números introducidos por el usuario
+            Console.Write("¿Cual es el numero 1?");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("¿Y el numero 2?");
+            int n2 = int.Parse(Console.ReadLine());
+            int Res = n1 + n2;
+            Console.WriteLine("La suma entre los 2 números ingresados es: " + Res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4_CHAUMONT_M/0_PC4_5/0_PC4_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC6_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Escribe un programa en C# que calcule el resultado de restar dos números introducidos por el usuario.
+            Console.Write("Ingrese el numero 1: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("E ingrese el numero 2: ");
+            int n2 = int.Parse(Console.ReadLine());
+            int Res = n1 - n2;
+            Console.WriteLine("El resultado de la resta es: " + Res);
+            Console.ReadKey();
+        }
+    }
+}
