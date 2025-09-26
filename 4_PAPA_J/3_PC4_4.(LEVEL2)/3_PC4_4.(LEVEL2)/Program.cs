951ebc95d27ccc6214f8664891001b0fbc491bb7
Noni <120880021+nonimiauchis@users.noreply.github.com>
2025-09-26 20:38:50 -0300

    Add files via upload

---
+++ 4_PAPA_J/3_PC4_4.(LEVEL2)/3_PC4_4.(LEVEL2)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_PC4_4._LEVEL2_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Nivel 2 – Ping Check (LITE)");
+            int[] p = { 13, 250, -5, 40, 40, 40, 100, 205, 100 }; // válidos: 13, 40, 100 en idx 0,3,6
+            int s = Level2.SumValidEveryThird(p);
+            bool ok = s == (13 + 40 + 100); // 153
+            Console.WriteLine(ok ? "✔ UNLOCK → Fragmento: -ACCESS" : "🔒 LOCKED");
+
+            Console.WriteLine("\nPresione Enter para salir...");
+            Console.ReadLine();
+        }
+    }
+
+    static class Level2
+    {
+        public static int SumValidEveryThird(int[] p)
+        {
+            int suma = 0;
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (i % 3 == 0) 
+                {
+                    if (p[i] >= 0 && p[i] <= 200) 
+                    {
+                        suma += p[i];
+                    }
+                }
+            }
+
+            return suma;
+        }
+    }
+}
